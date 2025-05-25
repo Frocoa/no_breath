225550b5cea4e2fcfcd7fd78dfb514388ad7de26
Frocoa <Frocoa@gmail.com>
2025-05-25 01:46:35 -0400

    Plants growing animation

--- Assets/Scripts/Crops/Crop.cs
+++ Assets/Scripts/Crops/Crop.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Crop : MonoBehaviour
 {
+
+    [SerializeField]
+    private Sprite[] growthStageSprites;
+    private SpriteRenderer spriteRenderer;
     private int growthStage = 0;
     private int timeToGrow = 5;
     private int stageProgress = 0;
+    private int maxGrowthStage = 5;
 
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void Start()
     {
         TickManager.Instance.SubscribeToRandomTick(Grow);
@@ -15,11 +25,14 @@ public class Crop : MonoBehaviour
 
     private void Grow()
     {
+        if (growthStage >= maxGrowthStage) return;
+
         stageProgress++;
         if (stageProgress >= timeToGrow)
         {
             growthStage++;
             stageProgress = 0;
+            spriteRenderer.sprite = growthStageSprites[growthStage];
             Debug.Log("Crop has grown to stage: " + growthStage);
         }
     }
